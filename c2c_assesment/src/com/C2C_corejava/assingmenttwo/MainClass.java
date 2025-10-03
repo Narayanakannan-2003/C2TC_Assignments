b6e16088de28f2910ba3a065f36b95edccac3ce0
Narayanan.G <Narayanan.G@LAPTOP-GFIKT0GL>
2025-10-03 22:14:59 +0530

    commit

---
+++ c2c_assesment/src/com/C2C_corejava/assingmenttwo/MainClass.java
@@ -0,0 +1,16 @@
+package com.C2C_corejava.assingmenttwo;
+
+public class MainClass {
+	
+		public static void main(String[] args) {
+		   // Demonstrate default constructor
+		   Student s = new Student();
+
+		   // Create Commission object and test methods
+		   Commission c = new Commission();
+		   c.acceptDetails();
+		   c.calculateCommission();
+		}
+		}
+
+
---
+++ c2c_assesment/src/com/C2C_corejava/assingmenttwo/Student.java
@@ -0,0 +1,9 @@
+package com.C2C_corejava.assingmenttwo;
+
+public class Student {
+	 public Student() {
+	        System.out.println("Student object is created");
+	    }
+	}
+
+
